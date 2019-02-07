378745fd6f93079a4d548648219d460fb7637050
“Simaxa7” <simaxa7@mail.ru>
2019-02-07 18:56:18 +0300

    feat: add todo-list in the gh-pages

---
+++ precache-manifest.190a8442126baa9cfa3078b51dd836c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "abf37d0ae4ab9ac4d045",
+    "url": "/static/js/main.abf37d0a.chunk.js"
+  },
+  {
+    "revision": "86fd2c6431b26deabcf5",
+    "url": "/static/js/1.86fd2c64.chunk.js"
+  },
+  {
+    "revision": "abf37d0ae4ab9ac4d045",
+    "url": "/static/css/main.b5740b20.chunk.css"
+  },
+  {
+    "revision": "4194d6b3e0d4d231a76be9d918833f05",
+    "url": "/index.html"
+  }
+];
